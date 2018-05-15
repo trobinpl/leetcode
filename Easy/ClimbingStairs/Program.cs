aca8a97b10fbe30e433326416d807d790c140d44
trobinpl <pawel+github@hemperek.pl>
2018-05-15 10:30:05 +0200

    ClimbingStairs and RemoveDuplicatesFromSortedList problems solved

---
+++ Easy/ClimbingStairs/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClimbingStairs
+{
+    class Program
+    {
+        /*
+         * You are climbing a stair case. It takes n steps to reach to the top.
+
+           Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+           Note: Given n will be a positive integer.
+
+            2 -> 2 (1+1; 2)
+            3 -> 3 (1+1+1, 1+2. 2+1)
+        */
+        static void Main(string[] args)
+        {
+            //int r = AllPossibleWays(2);
+            //int r2 = AllPossibleWays(3);
+            int r3 = ClimbStairs(4);
+            int r4 = ClimbStairs(5);
+            int r5 = ClimbStairs(6);
+        }
+
+        static int ClimbStairs(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            int[] ways = new int[n + 1];
+            ways[1] = 1;
+            ways[2] = 2;
+            for (int i = 3; i <= n; i++)
+            {
+                ways[i] = ways[i - 1] + ways[i - 2];
+            }
+            return ways[n];
+        }
+    }
+}
---
+++ Easy/RemoveDuplicatesFromSortedList/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveDuplicatesFromSortedList
+{
+    class Program
+    {
+        /*
+         * Given a sorted linked list, delete all duplicates such that each element appear only once.
+         * 1->1->2 --> 1->2
+         * 1->1->2->3->3 --> 1->2->3
+         */
+        static void Main(string[] args)
+        {
+            ListNode l5 = new ListNode(3);
+            ListNode l4 = new ListNode(3);
+            ListNode l3 = new ListNode(2);
+            ListNode l2 = new ListNode(1);
+            ListNode l1 = new ListNode(1);
+            l4.next = l5;
+            l3.next = l4;
+            l2.next = l3;
+            l1.next = l2;
+
+            ListNode r = DeleteDuplicates(l1);
+        }
+
+        static ListNode DeleteDuplicates(ListNode head)
+        {
+            ListNode current = head;
+            while(current != null && current.next != null)
+            {
+                if(current.val == current.next.val)
+                {
+                    current.next = current.next.next;
+                }
+                else
+                {
+                    current = current.next;
+                }
+            }
+            return head;
+        }
+    }
+
+    public class ListNode
+    {
+        public int val { get; set; }
+        public ListNode next { get; set; }
+        public ListNode(int x)
+        {
+            this.val = x;
+        }
+    }
+}
