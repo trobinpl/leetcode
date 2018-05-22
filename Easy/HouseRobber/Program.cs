c8247b71246f577518b7dbc1b9abe45279da946c
trobinpl <pawel+github@hemperek.pl>
2018-05-22 14:57:42 +0200

    NumberOf1Bits problem solved. First attempt on HouseRobber problem

---
+++ Easy/HouseRobber/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseRobber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int r = Rob(new int[] { 1, 2, 3, 1 });
+            int r2 = Rob(new int[] { 2, 7, 9, 3, 1 });
+            int r3 = Rob(new int[] { });
+            int r4 = Rob(new int[] { 1 });
+        }
+
+        static public int Rob(int[] nums)
+        {
+            int firstSum = 0;
+            int secondSum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if(i % 2 == 0)
+                {
+                    firstSum += nums[i];
+                }
+                else
+                {
+                    secondSum += nums[i];
+                }
+            }
+            return Math.Max(firstSum, secondSum);
+        }
+    }
+}
---
+++ Easy/NumberOf1Bits/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberOf1Bits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int r = MyHammingWeight(11);
+            int r2 = MyHammingWeight(128);
+            int r3 = MyHammingWeight(128);
+            int r4 = MyHammingWeight(128);
+        }
+
+        static int HammingWeight(uint n)
+        {
+            int counter = 0;
+            for(int i = 0; i <= 31; i++)
+            {
+                if(((n >> i) & 1) == 1)
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+
+        static int MyHammingWeight(uint n)
+        {
+            int counter = 0;
+            while(n > 0)
+            {
+                if(n % 2 == 1)
+                {
+                    counter++;
+                }
+                n /= 2;
+            }
+            return counter;
+        }
+    }
+}
