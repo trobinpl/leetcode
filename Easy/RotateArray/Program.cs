16d0f0ef37ce3d94263ce5e561f64a52c187c015
trobinpl <pawel+github@hemperek.pl>
2018-05-22 12:50:16 +0200

    RotateArray problem solved

--- Easy/RotateArray/Program.cs
+++ Easy/RotateArray/Program.cs
@@ -10,11 +10,38 @@ namespace RotateArray
     {
         static void Main(string[] args)
         {
-            int[] nums1 = { 1, 2, 3, 4, 5 };
-            Rotate(nums1, 6);
+            int[] nums1 = { 1, 2, 3, 4, 5, 6 };
+            int[] nums2 = { 1 };
+            int[] nums3 = { 1, 2 };
+            //RotateRecursive(nums1, 6);
+            Rotate(nums1, 2);
+            Rotate(nums2, 0);
+            Rotate(nums3, 1);
         }
 
         static void Rotate(int[] nums, int k)
+        {
+            k %= nums.Length;
+
+            Reverse(nums, 0, nums.Length - 1);
+            Reverse(nums, 0, k - 1);
+            Reverse(nums, k, nums.Length - 1);
+
+        }
+
+        static void Reverse(int[] nums, int start, int end)
+        {
+            while(start < end)
+            {
+                int temp = nums[start];
+                nums[start] = nums[end];
+                nums[end] = temp;
+                start++;
+                end--;
+            }
+        }
+
+        static void RotateRecursive(int[] nums, int k)
         {
             if(nums.Length == 0)
             {
